b1fb1322f6d1763f17f8780a81ed815dda0e0391
Timothy Earley <timmy@earley.de>
2019-01-11 22:32:03 +0100

    Initial heroku + ktor

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+	kotlin("jvm") version "1.3.10"
+	id("com.github.johnrengelman.shadow") version "4.0.3"
+	application
+}
+
+repositories {
+	jcenter()
+}
+
+val ktor_version = "1.1.1"
+
+dependencies {
+
+	compile("io.ktor:ktor-server-netty:$ktor_version")
+	compile("ch.qos.logback:logback-classic:1.2.3")
+
+}
+
+application {
+	mainClassName = "de.earley.gogogo.backend.MainKt"
+}
+
+tasks {
+
+	withType<ShadowJar> {
+		baseName = "gogogo-backend"
+		classifier = ""
+		version = ""
+	}
+
+	val stage by registering {
+		dependsOn("clean", "shadowJar")
+	}
+
+}
