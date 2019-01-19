f58127e5599085a9310f97b79b03eea6945e945c
Timothy Earley <timmy@earley.de>
2019-01-19 14:28:33 +0100

    Removed gitVersion to fix heroku

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-plugins {
-	id("com.palantir.git-version") version "0.11.0"
-}
-
-val gitVersion: groovy.lang.Closure<*> by extra
-
 allprojects {
 	group = "de.earley.gogogo"
-	version = gitVersion()
+	version = "0.1.0"
 
 	repositories {
 		jcenter()
