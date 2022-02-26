d1209acc342913e7445f77613988b724f38b67d8
Timothy Earley <timmy@earley.de>
2022-02-26 05:52:14 +0100

    Fixed state hash

---
+++ gogogo-common/src/commonTest/kotlin/TestPossibleMoves.kt
@@ -0,0 +1,20 @@
+import de.earley.gogogo.game.State
+import de.earley.gogogo.game.withMove
+import kotlin.test.Test
+
+
+class TestPossibleMoves {
+
+    @Test fun testAllPossibleMovesArePossible() {
+        State.initial().testMovesToDepth(7)
+    }
+
+    private fun State.testMovesToDepth(depth: Int) {
+        if (depth == 0) return
+        for (move in possibleMoves) {
+            withMove(move) {
+                testMovesToDepth(depth - 1)
+            }
+        }
+    }
+}
